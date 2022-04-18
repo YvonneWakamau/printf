0735b6e08f76819262ffa17aa40e77e1f5759754
newtonkarumba <newtonkarumba@yahoo.com>
2022-04-18 22:26:21 +0300

    print

--- _printf.c
+++ _printf.c
@@ -14,9 +14,7 @@ int (*pfunc)(va_list, flags_t *);
 const char *p;
 va_list arguments;
 flags_t flags = {0, 0, 0};
-
 register int count = 0;
-
 va_start(arguments, format);
 if (!format || (format[0] == '%' && !format[1]))
 	return (-1);
@@ -24,11 +22,11 @@ if (format[0] == '%' && format[1] == ' ' && !format[2])
 	return (-1);
 for (p = format; *p; p++)
 {
-	if (*p == '%')
-	{
-	p++;
-	if (*p == '%')
-		{
+if (*p == '%')
+{
+p++;
+if (*p == '%')
+{
 count += _putchar('%');
 continue;
 }
